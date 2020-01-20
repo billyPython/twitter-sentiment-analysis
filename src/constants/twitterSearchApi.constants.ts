72d2c4f2a4ee63e35af785e9ee33b30b3083cd71
Danilo Vulicevic <danilo.vulicevic@yahoo.com>
2020-01-21 00:18:53 +0100

    remove twitter creds

--- src/constants/twitterSearchApi.constants.ts
+++ src/constants/twitterSearchApi.constants.ts
@@ -1,9 +1,9 @@
 import Twitter from 'twitter';
 
-export const TWITTER_KEY = 'GrV3FRnJsetQcG4MgXvEuZDMG';
-export const TWITTER_SECRET_KEY = 'IhkwSs0U9UP1lPm4pU143SIoLNGu5H2ZGL5OOgIhOmXxYPdYMB';
-export const TWITTER_ACCESS_TOKEN = '1211078127599812610-Fe9MatuBXTHfAHeJfdR563E408Lj3J';
-export const TWITTER_ACCESS_TOKEN_SECRET = 'wwlTUl9VwieBy0cJwVe2TFG0Ybfixt9lHoQKTp9s4vtj9';
+export const TWITTER_KEY = 'key';
+export const TWITTER_SECRET_KEY = 'secret key';
+export const TWITTER_ACCESS_TOKEN = 'token';
+export const TWITTER_ACCESS_TOKEN_SECRET = 'secret token';
 
 export const clientTwitter = new Twitter({
     consumer_key: TWITTER_KEY,
